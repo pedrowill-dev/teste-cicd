610f67cdce5fc0971b1c40172ce7740ef24baad8
Pedro Willian <pedro.willian@techmail.com.br>
2025-08-27 23:43:10 -0300

    pytest

--- tests/test_scrapping.py
+++ tests/test_scrapping.py
@@ -1,6 +1,15 @@
-from src.scrapping import get_title
+import requests
+from bs4 import BeautifulSoup
+
+def get_title(url: str) -> str:
+    response = requests.get(url, timeout=10)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup.title.string if soup.title else "Sem título"
 
 
 def test_google_title():
     title = get_title("https://www.google.com")
     assert "Google" in title
+
+
